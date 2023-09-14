ded3306ef5c5daf39d1bfc8fccda1cdcd6c07542
Vicoclico <victor.bonlieu@gmail.com>
2023-09-14 14:59:36 +0200

    création page de code en python

---
+++ Python_import.py
@@ -0,0 +1,5 @@
+import pandas as pd                     # for dataset manipulation (DataFrames)
+import sklearn.datasets                 # the datasets we are going to use
+import numpy as np                      # allows some mathematical operations
+import matplotlib.pyplot as plt         # library used to display graphs
+import seaborn as sns                   # more convenient visualization library for dataframes
